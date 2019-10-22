a1537fd8b29beafbdc8c307eaaa5a600ed38bdd8
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-10-22 14:05:53 +0200

    modified Vehicle

--- CarRental/src/main/java/at/htl/carRental/model/Vehicle.java
+++ CarRental/src/main/java/at/htl/carRental/model/Vehicle.java
@@ -1,4 +1,64 @@
 package at.htl.carRental.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Vehicle {
+
+    private String brand;
+    private String type;
+    private int constructionYear;
+    private int pivotalYear;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String brand, String type, int constructionYear, int pivotalYear) {
+        this.brand = brand;
+        this.type = type;
+        this.constructionYear = constructionYear;
+        this.pivotalYear = pivotalYear;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getConstructionYear() {
+        return constructionYear;
+    }
+
+    public void setConstructionYear(int constructionYear) {
+        this.constructionYear = constructionYear;
+    }
+
+    public int getPivotalYear() {
+        return pivotalYear;
+    }
+
+    public void setPivotalYear(int pivotalYear) {
+        this.pivotalYear = pivotalYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", type='" + type + '\'' +
+                ", constructionYear=" + constructionYear +
+                ", pivotalYear=" + pivotalYear +
+                '}';
+    }
 }
