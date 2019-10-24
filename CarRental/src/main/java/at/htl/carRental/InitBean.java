5db6cfef3eecabf74809d6a142e4bdf99cd2ecc6
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-10-24 19:21:13 +0200

    modified InitBean.java

---
+++ CarRental/src/main/java/at/htl/carRental/InitBean.java
@@ -0,0 +1,31 @@
+package at.htl.carRental;
+
+import at.htl.carRental.model.Vehicle;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
+public class InitBean {
+    @PersistenceContext
+    EntityManager em;
+    public void init(@Observes@Initialized(ApplicationScoped.class) Object init){
+
+        Vehicle audi = new Vehicle("Audi","A6",2011,2011);
+        em.persist(audi);
+        em.persist(new Vehicle("Tesla","Model 3",2017,2017));
+        em.persist(new Vehicle("Opel","Commodore",1980,1980));
+
+
+    }
+
+    public void tearDown(@Observes @Destroyed(ApplicationScoped.class) Object init){
+
+    }
+}
