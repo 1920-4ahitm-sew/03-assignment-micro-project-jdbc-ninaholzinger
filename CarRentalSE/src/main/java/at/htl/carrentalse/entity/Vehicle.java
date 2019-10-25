35cdf565aa0e10dfcc7ab3db8268e767162963a4
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-10-25 11:17:02 +0200

    modified Vehicle.java

---
+++ CarRentalSE/src/main/java/at/htl/carrentalse/entity/Vehicle.java
@@ -0,0 +1,62 @@
+package at.htl.carrentalse.entity;
+
+public class Vehicle {
+
+    private Long id;
+    private String brand;
+    private String type;
+    private int constructionYear;
+    private int pivotalYear;
+
+
+    public Vehicle() {
+    }
+
+    public Vehicle(Long id, String brand, String type, int constructionYear, int pivotalYear) {
+        this.id = id;
+        this.brand = brand;
+        this.type = type;
+        this.constructionYear = constructionYear;
+        this.pivotalYear = pivotalYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+   /* public void setId(Long id) {
+        this.id = id;
+    }*/
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getConstructionYear() {
+        return constructionYear;
+    }
+
+    public void setConstructionYear(int constructionYear) {
+        this.constructionYear = constructionYear;
+    }
+
+    public int getPivotalYear() {
+        return pivotalYear;
+    }
+
+    public void setPivotalYear(int pivotalYear) {
+        this.pivotalYear = pivotalYear;
+    }
+}
