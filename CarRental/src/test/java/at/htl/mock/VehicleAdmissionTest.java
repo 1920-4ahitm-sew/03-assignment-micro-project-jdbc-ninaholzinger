8aa1b59deff84a759e128bae533e209eac211eb1
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-12-15 17:21:04 +0100

    modified VehicleAdmission.java

--- CarRental/src/test/java/at/htl/mock/VehicleAdmissionTest.java
+++ CarRental/src/test/java/at/htl/mock/VehicleAdmissionTest.java
@@ -1,13 +1,26 @@
 package at.htl.mock;
 
 import at.htl.carRental.model.Vehicle;
+import at.htl.carRental.model.VehicleAdmission;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 public class VehicleAdmissionTest {
 
     @Test
     void Test001(){
-        Vehicle vehicle = new Vehicle();
+        //arrange
+        VehicleAdmission vehicles = new VehicleAdmission();
+        vehicles.vehicle = mock(List.class);
 
+        //act
+        vehicles.add("Commodore");
+
+        //assert
+        verify(vehicles.vehicle,times(1)).add("Commodore");
     }
 }
