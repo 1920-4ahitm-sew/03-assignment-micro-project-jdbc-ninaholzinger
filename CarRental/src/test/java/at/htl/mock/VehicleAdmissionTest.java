65b0d7df07968bdb9b87ce974186015e3b442513
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-12-15 16:00:15 +0100

    renamed VehicleAdmissionTest.java

---
+++ CarRental/src/test/java/at/htl/mock/VehicleAdmissionTest.java
@@ -0,0 +1,13 @@
+package at.htl.mock;
+
+import at.htl.carRental.model.Vehicle;
+import org.junit.jupiter.api.Test;
+
+public class VehicleAdmissionTest {
+
+    @Test
+    void Test001(){
+        Vehicle vehicle = new Vehicle();
+
+    }
+}
