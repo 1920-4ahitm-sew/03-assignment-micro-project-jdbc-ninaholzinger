bab0a087a31701934d56869562882b8d255f08d2
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-12-15 16:08:16 +0100

    modified VehicleAdmission.java

--- CarRental/src/main/java/at/htl/carRental/model/VehicleAdmission.java
+++ CarRental/src/main/java/at/htl/carRental/model/VehicleAdmission.java
@@ -1,8 +1,25 @@
 package at.htl.carRental.model;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class VehicleAdmission {
 
+    public List<String> vehicle = new LinkedList<>();
+
+    public VehicleAdmission() {
+    }
+
+    public void setVehicle(List<String> vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public List<String> getVehicle() {
+        return vehicle;
+    }
+
+    public void add(String name){
+        vehicle.add(name);
+    }
 
 }
