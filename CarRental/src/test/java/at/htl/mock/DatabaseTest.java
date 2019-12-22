227038f1ff60d00885dc7dd64776273ddf8ef552
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2019-12-22 17:13:26 +0100

    wrote first database test to check if table exists

--- CarRental/src/test/java/at/htl/mock/DatabaseTest.java
+++ CarRental/src/test/java/at/htl/mock/DatabaseTest.java
@@ -1,5 +1,30 @@
 package at.htl.mock;
 
+import org.assertj.db.type.Source;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLSyntaxErrorException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.db.output.Outputs.output;
+
 public class DatabaseTest {
 
+    public String url = "jdbc:derby://localhost:1527/db";
+    public String username = "app";
+    public String password = "app";
+
+    @Test
+    public void test_databaseTest(){
+       Source source = new Source(url,
+               username,
+               password);
+       Table table = new Table(source,"VEHICLE");
+        assertThatThrownBy(() -> {
+            output(table).toConsole();
+        });
+
+
+    }
 }
